f390db14664fac9270aaf483f37b108454962ac5
dhanyn10 <dhanyn.dhan@gmail.com>
2020-09-30 10:54:27 +0700

    add admin table

---
+++ database/migrations/2020_09_30_023712_admin.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\User as UserModel;
+class Admin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("user", function(Blueprint $table){
+            $table->increments("id");
+            $table->string("username");
+            $table->string("password");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("user");
+    }
+}
